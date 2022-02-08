9eaad4466d633b36fab267231161dbb3eef6e18b
mikewise5 <mike.wise@kellogg.northwestern.edu>
2022-02-08 04:41:57 +0000

    Commit 4

--- db/migrate/20220207022240_create_movies.rb
+++ db/migrate/20220207022240_create_movies.rb
@@ -4,7 +4,7 @@ class CreateMovies < ActiveRecord::Migration[7.0]
         t.string :title
         t.integer :year_released
         t.string :rated
-        t.string :person_id
+        t.integer :person_id
         
       t.timestamps
     end
--- db/migrate/20220207023627_create_roles.rb
+++ db/migrate/20220207023627_create_roles.rb
@@ -2,7 +2,7 @@ class CreateRoles < ActiveRecord::Migration[7.0]
   def change
     create_table :roles do |t|
         t.integer :movie_id
-        t.string :person_id
+        t.integer :person_id
         t.string :character_name
       t.timestamps
     end
