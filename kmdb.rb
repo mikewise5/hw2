7a96e9e35bd3361fca108cee87c3d48a3f60735e
mikewise5 <mike.wise@kellogg.northwestern.edu>
2022-02-08 04:38:59 +0000

    Commit 3

--- kmdb.rb
+++ kmdb.rb
@@ -87,7 +87,7 @@ Role.destroy_all
 values = { title: "Batman Begins", 
             year_released: "2005", 
             rated: "PG-13",
-            person_id: "1"
+            person_id: nolan.id
         }
 
 batman_begins = Movie.new(values)
@@ -97,14 +97,14 @@ new_movie = Movie.new
 new_movie.title = "The Dark Knight"
 new_movie.year_released = "2008"
 new_movie.rated = "PG-13"
-new_movie.person_id = "1"
+new_movie.person_id = nolan.id
 new_movie.save
 
 new_movie = Movie.new
 new_movie.title = "The Dark Knight Rises"
 new_movie.year_released = "2012"
 new_movie.rated = "PG-13"
-new_movie.person_id = "1"
+new_movie.person_id = nolan.id
 new_movie.save
 
 puts "There are now #{Movie.all.count} movies."
@@ -159,13 +159,56 @@ new_person = Person.new
 new_person.name = "Christopher Nolan"
 new_person.save
 
-##puts "There are now #{Person.all.count} people."
+puts "There are now #{Person.all.count} people."
 
 ##Creates people IDs
-bale = Role.where({ role: "Batman Begins"})[0]
-puts begins.id
-begins_id = begins.id
+bale = Person.where({ name: "Christian Bale"})[0]
+puts bale.id
+bale_id = bale.id
 
+caine = Person.where({ name: "Michael Caine"})[0]
+puts caine.id
+caine_id = caine.id
+
+neeson = Person.where({ name: "Liam Neeson"})[0]
+puts neeson.id
+neeson_id = neeson.id
+
+holmes = Person.where({ name: "Katie Holmes"})[0]
+puts holmes.id
+holmes_id = holmes.id
+
+oldman = Person.where({ name: "Gary Oldman"})[0]
+puts oldman.id
+oldman_id = oldman.id
+
+ledger = Person.where({ name: "Heath Ledger"})[0]
+puts ledger.id
+ledger_id = ledger.id
+
+eckhart = Person.where({ name: "Aaron Eckhart"})[0]
+puts eckhart.id
+eckhart_id = eckhart.id
+
+gyllenhaal = Person.where({ name: "Maggie Gyllenhaal"})[0]
+puts gyllenhaal.id
+gyllenhaal_id = gyllenhaal.id
+
+hardy = Person.where({ name: "Tom Hardy"})[0]
+puts hardy.id
+hardy_id = hardy.id
+
+levitt = Person.where({ name: "Joseph Gordon-Levitt"})[0]
+puts levitt.id
+levitt_id = levitt.id
+
+hathaway = Person.where({ name: "Anne Hathaway"})[0]
+puts hathaway.id
+hathaway_id = hathaway.id
+
+nolan = Person.where({ name: "Christopher Nolan"})[0]
+puts nolan.id
+nolan_id = nolan.id
 
 ##Creates IDs for movies
 
@@ -183,10 +226,89 @@ rises_id = rises.id
 
 role = Role.new
 role.movie_id = begins.id
-role.person_id = ""
+role.person_id = bale.id
 role.character_name= "Bruce Wayne"
 role.save
 
+role = Role.new
+role.movie_id = begins.id
+role.person_id = caine.id
+role.character_name= "Alfred"
+role.save
+
+role = Role.new
+role.movie_id = begins.id
+role.person_id = holmes.id
+role.character_name= "Rachel Dawes"
+role.save
+
+role = Role.new
+role.movie_id = begins.id
+role.person_id = oldman.id
+role.character_name= "Commissioner Gordon"
+role.save
+
+role = Role.new
+role.movie_id = dark_knight.id
+role.person_id = bale.id
+role.character_name= "Bruce Wayne"
+role.save
+
+role = Role.new
+role.movie_id = dark_knight.id
+role.person_id = ledger.id
+role.character_name= "Joker"
+role.save
+
+role = Role.new
+role.movie_id = dark_knight.id
+role.person_id = eckhart.id
+role.character_name= "Harvey Dent"
+role.save
+
+role = Role.new
+role.movie_id = dark_knight.id
+role.person_id = caine.id
+role.character_name= "Alfred"
+role.save
+
+role = Role.new
+role.movie_id = dark_knight.id
+role.person_id = gyllenhaal.id
+role.character_name= "Rachel Dawes"
+role.save
+
+role = Role.new
+role.movie_id = rises.id
+role.person_id = bale.id
+role.character_name= "Bruce Wayne"
+role.save
+
+role = Role.new
+role.movie_id = rises.id
+role.person_id = oldman.id
+role.character_name= "Commissioner Gordon"
+role.save
+
+role = Role.new
+role.movie_id = rises.id
+role.person_id = hardy.id
+role.character_name= "Bane"
+role.save
+
+role = Role.new
+role.movie_id = rises.id
+role.person_id = levitt.id
+role.character_name= "John Blake"
+role.save
+
+role = Role.new
+role.movie_id = rises.id
+role.person_id = hathaway.id
+role.character_name= "Selina Kyle"
+role.save
+
+puts "There are now #{Role.all.count} roles."
 
 
 #actor = Actor.new
@@ -203,6 +325,13 @@ puts ""
 
 # Query the movies data and loop through the results to display the movies output
 # TODO!
+
+all_movies = Movie.all
+
+for movie in all_movies
+    puts "#{movie.title} - #{movie.year_released} - #{movie.rated} - #{movie.person_id}"
+end
+
 
 # Prints a header for the cast output
 puts ""
